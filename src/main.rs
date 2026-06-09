b4012966c85181d9741d399ec4dcaaf33129efd7
Kenji Funaki <kf2623@nyu.edu>
2026-06-08 20:39:19 -0400

    Step 4: Add wallet key generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::time::Instant;
 use rust_mini_chain::blockchain::Blockchain;
 use rust_mini_chain::transaction::Transaction;
+use rust_mini_chain::wallet::Wallet;
 
 fn main() {
     let start = Instant::now();
+
+    let alice = Wallet::new();
+    let bob   = Wallet::new();
+
+#[cfg(debug_assertions)]
+{
+    println!(
+        "Alice pubkey: {:?}", alice.verifying_key
+    );
+    
+    println!(
+        "Bob pubkey: {:?}", bob.verifying_key
+    );
+}    
+    println!("Alice Hex pubkey: {}", alice.public_key_hex());
+    println!("Bob Hex pubkey  : {}", bob.public_key_hex());
 
     let difficulty = 4; //Added by Step 2
     let mut blockchain = Blockchain::new(difficulty);
---
+++ src/wallet.rs
@@ -0,0 +1,27 @@
+use ed25519_dalek::{
+    SigningKey,
+    VerifyingKey,
+};
+
+pub struct Wallet {
+    pub signing_key:   SigningKey,
+    pub verifying_key: VerifyingKey,
+}
+
+impl Wallet {
+    pub fn new() -> Self {
+        let secret = rand::random::<[u8; 32]>();
+
+        let signing_key  = SigningKey::from_bytes(&secret);
+        let verifying_key = signing_key.verifying_key();
+
+        Self {
+            signing_key,
+            verifying_key,
+        }
+    }
+
+    pub fn public_key_hex(&self) -> String {
+        hex::encode(self.verifying_key.to_bytes())
+    }
+}
